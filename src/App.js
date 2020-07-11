795161f9d43fe999a2a6a70d9d6ba79ce9bb4bf6
Fredrik Langå <fl222pw@student.lnu.se>
2020-07-11 15:10:19 +0200

    add homepage component and scss

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+
+import HomePage from './homepage.component'
 
 function App() {
   return (
     <div>
-      Hello World
+      <HomePage />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
