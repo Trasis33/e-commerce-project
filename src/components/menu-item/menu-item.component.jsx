d17a70552d831c51ccbdf792cd26304dcfb8dfdd
Fredrik Langå <fl222pw@student.lnu.se>
2020-07-12 13:50:38 +0200

    destructured directory and menuitem to individual components

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './menu-item.styles.scss'
+
+// eslint-disable-next-line react/prop-types
+const MenuItem = ({ title }) => (
+  <div className="menu-item">
+    <div className="content">
+      <h1 className="title">{title}</h1>
+      <span className="subtitle">SHOP NOW</span>
+    </div>
+  </div>
+)
+
+export default MenuItem
