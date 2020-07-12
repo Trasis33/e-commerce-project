1662193c3358f1c54942d4119aed2747799965ba
Fredrik Langå <fl222pw@student.lnu.se>
2020-07-12 15:51:53 +0200

    add font, bg images to menuItems, fix proptypes lint error

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
+import propTypes from 'prop-types'
 
 import './menu-item.styles.scss'
 
-// eslint-disable-next-line react/prop-types
-const MenuItem = ({ title }) => (
-  <div className="menu-item">
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div
+    style={{
+      backgroundImage: `url(${imageUrl})`,
+    }}
+    className={`${size} menu-item`}
+  >
     <div className="content">
-      <h1 className="title">{title}</h1>
+      <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
 )
 
 export default MenuItem
+
+MenuItem.propTypes = {
+  title: propTypes.string,
+  imageUrl: propTypes.string,
+  size: propTypes.string,
+}
