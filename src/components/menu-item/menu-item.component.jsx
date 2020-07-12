239732dafe1ba3bb25bc3b5809d71d4b7305dcbb
Fredrik Langå <fl222pw@student.lnu.se>
2020-07-12 22:07:48 +0200

    implement react router, menuItems nav withRouter HOC

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,17 @@
+/* eslint-disable no-restricted-globals */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React from 'react'
 import propTypes from 'prop-types'
+import { withRouter } from 'react-router-dom'
 
 import './menu-item.styles.scss'
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
     {/* Separate div because of image zoom effect to stay inside container */}
     <div
       className="background-image"
@@ -19,10 +26,13 @@ const MenuItem = ({ title, imageUrl, size }) => (
   </div>
 )
 
-export default MenuItem
+export default withRouter(MenuItem)
 
 MenuItem.propTypes = {
   title: propTypes.string,
   imageUrl: propTypes.string,
   size: propTypes.string,
+  linkUrl: propTypes.string,
+  history: propTypes.object,
+  match: propTypes.object,
 }
