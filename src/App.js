852a8a887dc17fc67822da963281f611fe38757c
Fredrik Langå <fl222pw@student.lnu.se>
2020-07-18 20:03:09 +0200

    add header and signin-signup page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import './App.css'
 
+import Header from './components/header/header.component'
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import SignInAndSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component'
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
         <Route path="/shop" component={ShopPage}></Route>
+        <Route path="/signIn" component={SignInAndSignUpPage}></Route>
       </Switch>
     </div>
   )
