40afc5868a3d43917eb685667c29705ef9124bac
Fredrik Langå <fl222pw@student.lnu.se>
2020-07-31 22:28:03 +0200

    add collectionsOverview, move logic from shopPage to collectionsOverview component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,25 +1,11 @@
 import React from 'react'
-import propTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { createStructuredSelector } from 'reselect'
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component'
-import { selectCollections } from '../../redux/shop/shop.selectors'
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
 
-const ShopPage = ({ collections }) => (
+const ShopPage = () => (
   <div className="shop-page">
-    {collections.map(({ id, ...otherCollectionProps }) => (
-      <CollectionPreview key={id} {...otherCollectionProps} />
-    ))}
+    <CollectionsOverview />
   </div>
 )
 
-ShopPage.propTypes = {
-  collections: propTypes.array,
-}
-
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
-})
-
-export default connect(mapStateToProps)(ShopPage)
+export default ShopPage
