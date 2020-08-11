fecaef2013281168f522f640a6a07f0de7f04e60
Fredrik Langå <fl222pw@student.lnu.se>
2020-08-11 19:12:09 +0200

    remove scss files

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 import { HomePageContainer } from './homepage.styles'
 
-import './homepage.styles.scss'
 import Directory from '../../components/directory/directory.component'
 
 const HomePage = () => (
